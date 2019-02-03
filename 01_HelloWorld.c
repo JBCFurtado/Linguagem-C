0ff5e51077fa09d9e24966d0d21a330006112db3
Jobs <josuebatista.ti@gmail.com>
2019-02-03 14:14:17 -0300

    03022019

--- 01_HelloWorld.c
+++ 01_HelloWorld.c
@@ -1,3 +1,11 @@
+/*
+*INSTITUTO FEDERAL DE EDUCAÇÃO, CIÊNCIA E TECNOLOGIA DO PARÁ
+*Campus Altamira
+*Tecnologia em Análise e Desenvolvimento de Sistemas
+*Estrutura de Dados
+*Autor: Jobs
+*Área e Diâmetro de uma circunferência
+*/
 #include <stdio.h>
 int main(){
    printf("Ola mundo!");
--- 03_Área e Diâmetro de um Círculo.c
+++ 03_Área e Diâmetro de um Círculo.c
@@ -1,6 +1,10 @@
 /*
-*Área e Diâmetro de uma circunferência
+*INSTITUTO FEDERAL DE EDUCAÇÃO, CIÊNCIA E TECNOLOGIA DO PARÁ
+*Campus Altamira
+*Tecnologia em Análise e Desenvolvimento de Sistemas
+*Estrutura de Dados
 *Autor: Jobs
+*Área e Diâmetro de uma circunferência
 */
 #include <stdio.h>
 int main(){
